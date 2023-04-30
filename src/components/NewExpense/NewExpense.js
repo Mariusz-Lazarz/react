72e747146947e46be4137ee508f85b4a15783166
Mariusz Łazarz <mariusz.lazarz97@gmail.com>
2023-04-30 20:09:39 +0200

    adding click events to form functionality

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
-const NewExpense = () => {
-    return <div className='new-expense'>
-        <ExpenseForm />
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+    setConditionForm(false)
+  };
+  const [conditionForm, setConditionForm] = useState(false);
+  const startEditingHandler = () => {
+    setConditionForm(true);
+  };
+  const stopEditingHandler = () => {
+    setConditionForm(false)
+  }
+  return (
+    <div className="new-expense">
+      {!conditionForm && <button className="new-expense" onClick={startEditingHandler}>
+        Add new expense
+      </button>}
+      {conditionForm && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/>}
+      
     </div>
+  );
 };
 
 export default NewExpense;
