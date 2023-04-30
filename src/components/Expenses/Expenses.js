53147a68c84e963661c4c42a5060e17b9247cbe9
Mariusz Łazarz <mariusz.lazarz97@gmail.com>
2023-04-30 22:00:18 +0200

    finishing simple todo

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ExpenseList from "./ExpenseList";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
@@ -17,7 +18,8 @@ function Expenses(props) {
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filterYear} onGetFilterData={getFilterData} />
-        <ExpenseList items={filteredExpenses}/>
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpenseList items={filteredExpenses} />
       </Card>
     </div>
   );
